8f58e237b0e757548d3d16fbdec96f8f21a9fe4a
Breno <joaobaise@hotmail.com>
2026-01-11 11:01:21 +0000

    ProductPage, IndexPage, primary buttons, general classes

--- src/app/index-page/index-page.ts
+++ src/app/index-page/index-page.ts
@@ -1,15 +1,15 @@
-import { Component,  } from '@angular/core';
-
+import { Component, inject, } from '@angular/core';
+import { RouterLink } from "@angular/router";
+import { ProductService } from '../product-service/product-service';
+import { ProductCard } from '../product-card/product-card';
 @Component({
   selector: 'app-index-page',
-  imports: [],
-  template: `
-    <p>
-      index-page works!
-    </p>
-  `,
+  imports: [ProductCard],
+  templateUrl: './index-page.html',
   styleUrl: './index-page.css',
 })
 export class IndexPage {
+  private ProductService = inject(ProductService);
+  products = this.ProductService.getProductList();
 
 }
--- src/app/product-service/product-service.ts
+++ src/app/product-service/product-service.ts
@@ -8,8 +8,8 @@ export class ProductService {
     {
       id: 0,
       prodName: 'IPhone 17 Pro Max',
-      prodPicNameSmall: '/assets/iphone.png',
-      prodPicNameLarge: '',
+      prodPicNameSmall: 'iphone17.jpg',
+      prodPicNameLarge: 'iphone17.jpg',
       prodDescriptionShort: 'The Latest IPhone',
       prodDescriptionLong: 'The new iphone has an m5 chip with 600gb of ram',
       prodPrice: 1050.99,
@@ -18,11 +18,21 @@ export class ProductService {
     {
       id: 1,
       prodName: 'IPhone 16 Pro Max',
-      prodPicNameSmall: '/assets/iphone.png',
-      prodPicNameLarge: '',
+      prodPicNameSmall: 'iphone16.jpg',
+      prodPicNameLarge: 'iphone16.jpg',
       prodDescriptionShort: 'The Second Latest IPhone',
       prodDescriptionLong: 'The new iphone has an m5 chip with 600gb of ram',
       prodPrice: 950.99,
+      prodQuantity: 6
+    },
+    {
+      id: 2,
+      prodName: 'Apple Macbook Air ',
+      prodPicNameSmall: '',
+      prodPicNameLarge: 'macbook.jpg',
+      prodDescriptionShort: 'All in Apple Desktop',
+      prodDescriptionLong: '500GB Hard disk, 32gb RAM, and much more.',
+      prodPrice: 2050.99,
       prodQuantity: 6
     },
     {
